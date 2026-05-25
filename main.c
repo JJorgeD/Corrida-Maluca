b3d50b8a862da9bfb5056305cb5083dbe1f32375
guilhermebaltar <gcb@cesar.school>
2026-05-25 17:47:37 -0300

    Add speed verification function for players

--- main.c
+++ main.c
@@ -111,3 +111,21 @@ void adicionarCarro(Carro **lista, int faixa, int y, int direcao) {
     novo->prox = *lista;
     *lista = novo;
 }
+void verificarVelocidade(Jogador *j, int frames, int *frames_mensagem, char *mensagem) {
+    int nova_velocidade = j->velocidade;
+
+    if      (frames >= 450 && j->velocidade < 10) { nova_velocidade = 10; strcpy(mensagem, "*** VELOCIDADE MAXIMA! ***"); }
+    else if (frames >= 400 && j->velocidade < 9)  { nova_velocidade = 9;  strcpy(mensagem, "*** VELOCIDADE 9! ***"); }
+    else if (frames >= 350 && j->velocidade < 8)  { nova_velocidade = 8;  strcpy(mensagem, "*** VELOCIDADE 8! ***"); }
+    else if (frames >= 300 && j->velocidade < 7)  { nova_velocidade = 7;  strcpy(mensagem, "*** VELOCIDADE 7! ***"); }
+    else if (frames >= 250 && j->velocidade < 6)  { nova_velocidade = 6;  strcpy(mensagem, "*** VELOCIDADE 6! ***"); }
+    else if (frames >= 200 && j->velocidade < 5)  { nova_velocidade = 5;  strcpy(mensagem, "*** VELOCIDADE 5! ***"); }
+    else if (frames >= 150 && j->velocidade < 4)  { nova_velocidade = 4;  strcpy(mensagem, "*** VELOCIDADE 4! ***"); }
+    else if (frames >= 100 && j->velocidade < 3)  { nova_velocidade = 3;  strcpy(mensagem, "*** VELOCIDADE 3! ***"); }
+    else if (frames >= 50  && j->velocidade < 2)  { nova_velocidade = 2;  strcpy(mensagem, "*** VELOCIDADE 2! ***"); }
+
+    if (nova_velocidade > j->velocidade) {
+        j->velocidade = nova_velocidade;
+        *frames_mensagem = 25;
+    }
+}
